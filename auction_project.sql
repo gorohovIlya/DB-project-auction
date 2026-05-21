a08f7d30767711836c231f8adac0f575448e85bb
gorohovIlya <ilya.gorohov.404@gmail.com>
2026-05-21 14:12:59 +0800

    feat: add tables creation requests

---
+++ auction_project.sql
@@ -0,0 +1,49 @@
+CREATE TABLE IF NOT EXISTS `lots` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(511),
+    PRIMARY KEY(`id`)
+    );
+    
+CREATE TABLE IF NOT EXISTS `min_stakes` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `lot_id` INT UNSIGNED NOT NULL,
+    `costs` DECIMAL(15, 2) NOT NULL,
+    `step` DECIMAL(15, 2) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY (`lot_id`),
+    FOREIGN KEY (`lot_id`) REFERENCES lots(id)
+    ON DELETE CASCADE
+    );
+
+CREATE TABLE IF NOT EXISTS `clients` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `lastname` VARCHAR(255) NOT NULL,
+    PRIMARY KEY(`id`)
+    );
+
+CREATE TABLE IF NOT EXISTS `statuses` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `status` VARCHAR(255),
+    PRIMARY KEY(`id`)
+);   
+
+CREATE TABLE IF NOT EXISTS `stake_history` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `min_stake_id` INT UNSIGNED NOT NULL,
+    `client_id` INT UNSIGNED NOT NULL,
+    `created_at` DATETIME,
+    `money_amount` DECIMAL(15, 2)
+    `status_code` INT UNSIGNED NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`min_stake_id`) REFERENCES min_stakes(`id`),
+    FOREIGN KEY (`client_id`) REFERENCES clients(`id`),
+    FOREIGN KEY (`status_code`) REFERENCES statuses(`id`),
+    ON DELETE CASCADE
+    );
+    
+    
+
+
+
